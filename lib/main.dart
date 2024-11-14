86c69daf7323a7c0344ad35dcbba2360aa8960ab
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2024-11-14 22:17:19 +0900

    feat: 最小のwindowサイズを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  if (kIsWeb || !Platform.isMacOS) {
+    throw UnsupportedError('This platform is not supported');
+  }
+
+  WidgetsFlutterBinding.ensureInitialized();
+  setWindowMinSize(const Size(640, 360));
   runApp(const MainApp());
 }
 
