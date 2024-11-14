6367371492fdeb8e867d35d463aa7bb9e534b6ae
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2024-11-14 21:05:08 +0900

    feat: window_size プラグインを追加

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import screen_retriever_macos
 import window_manager_plus
+import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   WindowManagerPlusPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlusPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
