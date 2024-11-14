252651f4555a71725f4e48f506dbbd19669e185c
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2024-11-14 22:35:10 +0900

    feat: OperationPanelApp と SlideShowApp の新規作成

---
+++ lib/operation_panel.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class OperationPanelApp extends StatelessWidget {
+  const OperationPanelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/slide_show_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SlideShowApp extends StatelessWidget {
+  const SlideShowApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
