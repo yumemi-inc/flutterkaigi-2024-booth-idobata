b9edf9a1da37fe5603b5999b51ab20d52e4732ff
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2024-11-14 22:53:01 +0900

    feat: 複数のwindowを表示可能に

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,44 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:idobata/operation_panel.dart';
+import 'package:idobata/slide_show_app.dart';
+import 'package:window_manager_plus/window_manager_plus.dart';
 import 'package:window_size/window_size.dart';
 
-void main() {
+const _slideShowAppWindowId = 0;
+
+Future<void> main(List<String> args) async {
   if (kIsWeb || !Platform.isMacOS) {
     throw UnsupportedError('This platform is not supported');
   }
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  final windowId = args.isEmpty
+      ? _slideShowAppWindowId
+      : int.tryParse(args[0]) ?? _slideShowAppWindowId;
+
+  await WindowManagerPlus.ensureInitialized(windowId);
   setWindowMinSize(const Size(640, 360));
-  runApp(const MainApp());
-}
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const windowOptions = WindowOptions(
+    size: Size(640, 360),
+    center: true,
+  );
+  unawaited(
+    WindowManagerPlus.current.waitUntilReadyToShow(windowOptions, () async {
+      await WindowManagerPlus.current.show();
+      await WindowManagerPlus.current.focus();
+    }),
+  );
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  final app = switch (windowId) {
+    _slideShowAppWindowId => const SlideShowApp(),
+    _ => const OperationPanelApp(),
+  };
+
+  runApp(app);
 }
--- lib/slide_show_app.dart
+++ lib/slide_show_app.dart
@@ -1,14 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:window_manager_plus/window_manager_plus.dart';
 
 class SlideShowApp extends StatelessWidget {
   const SlideShowApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: TextButton(
+            onPressed: () async {
+              unawaited(
+                WindowManagerPlus.createWindow(),
+              );
+            },
+            child: const Text('Hello World!'),
+          ),
         ),
       ),
     );
