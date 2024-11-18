c01b496d59fa7f87465f394029554101731cd36b
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2024-11-18 23:01:33 +0900

    fix: applicationSupportsSecureRestorableState を有効

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -7,4 +7,8 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return NSApp.windows.filter({$0 is MainFlutterWindow || $0 is WindowManagerPlusFlutterWindow}).count == 1 // or return false
   }
+
+  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
 }
