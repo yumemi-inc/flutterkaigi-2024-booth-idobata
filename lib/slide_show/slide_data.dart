d9d49b6b57a72dcae66c86482ffd89ffcec2a075
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2024-11-18 20:00:47 +0900

    feat: 仮のスライドデータを作成

--- lib/slide_show/slide_data.dart
+++ lib/slide_show/slide_data.dart
@@ -1,13 +1,29 @@
+final allSlides = [
+  const VideoData(
+    title: 'テンプレートプロジェクト CM',
+    assetPath: 'assets/commercial.mp4',
+  ),
+  const TalkData(
+    title: 'テンプレートプロジェクトについて',
+    overview: 'テンプレートプロジェクトについて何でもお話します！',
+    talker: 'blendthink',
+    talkerIconAssetPath: 'assets/blendthink.png',
+    talkTimeMinutes: 10,
+  ),
+];
+
 sealed class SlideData {
   const SlideData();
 }
 
 final class VideoData extends SlideData {
   const VideoData({
-    required this.url,
+    required this.title,
+    required this.assetPath,
   });
 
-  final String url;
+  final String title;
+  final String assetPath;
 }
 
 final class TalkData extends SlideData {
@@ -15,13 +31,13 @@ final class TalkData extends SlideData {
     required this.title,
     required this.overview,
     required this.talker,
-    required this.talkerImageUrl,
+    required this.talkerIconAssetPath,
     required this.talkTimeMinutes,
   });
 
   final String title;
   final String overview;
   final String talker;
-  final String talkerImageUrl;
+  final String talkerIconAssetPath;
   final int talkTimeMinutes;
 }
