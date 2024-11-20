e47c7394f47c31f8f037e902b73d0636aa8cf488
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-11-21 07:17:10 +0900

    feat: トークスライド画面のレイアウトを調整

--- lib/slide_show/screen/talk_slide_screen.dart
+++ lib/slide_show/screen/talk_slide_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:idobata/slide_show/slide_data.dart';
 
@@ -81,7 +84,7 @@ class _BackgroundTalkerImage extends StatelessWidget {
   }
 }
 
-class _TalkContent extends StatelessWidget {
+class _TalkContent extends HookWidget {
   const _TalkContent({
     required String title,
     required String talker,
@@ -96,67 +99,84 @@ class _TalkContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        left: 48,
-        right: MediaQuery.of(context).size.width / 2,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          FittedBox(
-            child: Text(
-              _title,
-              style: const TextStyle(
-                fontSize: 48,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                shadows: [
-                  Shadow(
-                    color: Colors.black54,
-                    offset: Offset(2, 2),
-                    blurRadius: 4,
+    final first = useState(true);
+    useEffect(
+      () {
+        final timer = Timer.periodic(
+          const Duration(seconds: 5),
+          (timer) {
+            if (context.mounted) {
+              first.value = false;
+              timer.cancel();
+            }
+          },
+        );
+        return timer.cancel;
+      },
+      const [],
+    );
+
+    const textStyle = TextStyle(
+      fontSize: 20,
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      shadows: [
+        Shadow(
+          color: Colors.black54,
+          offset: Offset(2, 2),
+          blurRadius: 4,
+        ),
+      ],
+    );
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final frameHeight = constraints.maxHeight;
+        final frameWidth = constraints.maxWidth;
+        return Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: frameWidth * 1 / 24,
+            ),
+            child: SizedBox(
+              height: frameHeight * 3 / 4,
+              width: frameWidth * 3.2 / 5,
+              child: Stack(
+                children: [
+                  AnimatedCrossFade(
+                    duration: const Duration(seconds: 3),
+                    firstChild: SizedBox(
+                      height: frameHeight * 3 / 4,
+                      child: Center(
+                        child: Text(
+                          _title,
+                          style: textStyle.copyWith(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                    ),
+                    secondChild: Text(
+                      _overview,
+                      style: textStyle,
+                    ),
+                    crossFadeState: first.value
+                        ? CrossFadeState.showFirst
+                        : CrossFadeState.showSecond,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      _talker,
+                      style: textStyle,
+                    ),
                   ),
                 ],
               ),
             ),
           ),
-          const SizedBox(height: 24),
-          Text(
-            _talker,
-            style: const TextStyle(
-              fontSize: 32,
-              color: Colors.white,
-              fontWeight: FontWeight.w500,
-              shadows: [
-                Shadow(
-                  color: Colors.black54,
-                  offset: Offset(2, 2),
-                  blurRadius: 4,
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 32),
-          Flexible(
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 24,
-              ),
-              child: Text(
-                _overview,
-                style: const TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  height: 1.4,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
